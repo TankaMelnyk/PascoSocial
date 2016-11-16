d5fd006aef01483d6f1e136187cf24411455d881
TankaMelnyk <tankamelnik@gmail.com>
2016-11-16 13:19:03 +0200

    Start of the project.

---
+++ Pasco_Free_Sample/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp      = require('gulp'), // Подключаем Gulp
+    sass        = require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
+
+gulp.task('scss', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'scss'], function() {
+    gulp.watch('app/sass/**/*.scss', ['scss']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
+});
